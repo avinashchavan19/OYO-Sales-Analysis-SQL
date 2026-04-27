3550b51523bfd7f04dd0fcd81e0627f8bc1c4ae6
Avinash Chavan <avinashchavan1917@gmail.com>
2026-04-27 13:48:47 +0530

    Update 01_database_setup.sql

--- Analysis/01_database_setup.sql
+++ Analysis/01_database_setup.sql
@@ -1,3 +1,11 @@
-
+-- DATABASE CREATION
 CREATE DATABASE oyo_sales;
 USE oyo_sales;
+
+-- DATA PREVIEW
+SELECT * FROM oyo_sales_csv;
+SELECT * FROM oyo_city_csv;
+
+-- DATA UNDERSTANDING
+DESCRIBE oyo_sales_csv;
+DESCRIBE oyo_city_csv;

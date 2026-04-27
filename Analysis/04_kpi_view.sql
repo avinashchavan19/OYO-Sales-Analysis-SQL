44e774b091544c21a003358b037a35103013ddf8
Avinash Chavan <avinashchavan1917@gmail.com>
2026-04-27 13:52:11 +0530

    Update 04_kpi_view.sql

--- Analysis/04_kpi_view.sql
+++ Analysis/04_kpi_view.sql
@@ -1,1 +1,16 @@
+-- KPI View
+CREATE VIEW sales_view AS
+SELECT 
+    COUNT(*) AS number_of_customers,
+    MAX(amount) AS maximum_sales,
+    MIN(amount) AS minimum_sales,
+    MIN(discount) AS minimum_discount,
+    MAX(discount) AS maximum_discount,
+    SUM(no_of_rooms) AS total_rooms,
+    AVG(amount) AS average_sales,
+    AVG(discount) AS average_discount
+FROM oyo_sales_csv;
+
+-- View Data
+SELECT * FROM sales_view;
 

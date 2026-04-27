048916c0eeb3e53aab87f7157780917d596ae872
Avinash Chavan <avinashchavan1917@gmail.com>
2026-04-27 13:51:48 +0530

    Update 03_constraints.sql

--- Analysis/03_constraints.sql
+++ Analysis/03_constraints.sql
@@ -1,1 +1,13 @@
 
+-- Primary Key (City Table)
+ALTER TABLE oyo_city_csv
+ADD PRIMARY KEY (hotel_id);
+
+-- Primary Key (Sales Table)
+ALTER TABLE oyo_sales_csv
+ADD PRIMARY KEY (booking_id);
+
+-- Foreign Key Relationship
+ALTER TABLE oyo_sales_csv
+ADD FOREIGN KEY (hotel_id)
+REFERENCES oyo_city_csv(hotel_id);
